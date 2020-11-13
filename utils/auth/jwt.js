64b7d70c959fa7bd2a004c78d632a8694118e561
qxth <sendnudesstrafe@gmail.com>
2020-11-12 22:51:52 -0500

    initial commit

--- utils/auth/jwt.js
+++ utils/auth/jwt.js
@@ -23,27 +23,3 @@ passport.use(new Strategy(opts,  (payload, done) => {
     
     }));
 
-// passport.use(
-//     new Strategy({
-    //     secretOrKey: `${process.env.TOKEN}`,
-    //     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    //     passReqToCallback: true
-    // },
-//     async (jwt_payload, done) => {
-//         try {
-//             const user = await User.findOne({'username': jwt_payload.email})
-//             console.log(jwt_payload.sub)
-//             if (!user){
-//                 return console.log('Unautorizado')
-//             }
-//             else{
-//                 return console.log('autorizado')
-//             }
-//         }
-//         catch(e){
-//             return console.log(e)
-//         }
-//     }
-
-//     )
-// )
